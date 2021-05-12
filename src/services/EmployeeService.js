fc9ce2fe92f26873bd1bd59c7071668989b1a1c1
root <root@localhost.localdomain>
2021-05-12 00:08:00 -0500

    server ip

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 //const EMPLOYEE_API_BASE_URL = "http://35.223.20.167:8145/api/auth/estados";
 //=======
 //const EMPLOYEE_API_BASE_URL = "http://35.223.20.167:8085/api/auth/estados";
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/auth/estados";
+const EMPLOYEE_API_BASE_URL = "http://34.122.12.31:8145/api/auth/estados";
 //>>>>>>> dcbbd65485472ffb46ee02dd2a494467c9a597e0
 
 class EmployeeService {
