57126d456f8e9fbccec5666e50d300c1b3132d3b
ImpersPv <A01730067@itesm.mx>
2021-05-12 00:29:10 -0500

    Vistas

---
+++ build/precache-manifest.9771f150a05298bddec8f0ea19918309.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18340534935c47c7009d5f13a650729e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "381877d9f450cf397fd6",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7c5eea2bd3a1de12ee12",
+    "url": "/static/css/main.c96ffa69.chunk.css"
+  },
+  {
+    "revision": "381877d9f450cf397fd6",
+    "url": "/static/js/2.bf058b54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bf058b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5eea2bd3a1de12ee12",
+    "url": "/static/js/main.1d3eb10c.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  }
+]);
