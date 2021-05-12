c34365b61a0ad2b1eef56e39db251b3cd61ba074
root <root@localhost.localdomain>
2021-05-12 00:12:09 -0500

    npm build

---
+++ build/precache-manifest.8f62deac12e1563c2d53f3406d1cb81c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a349580b4564504635e1e287c549ebdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "381877d9f450cf397fd6",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d8999acc56ade91e08cd",
+    "url": "/static/css/main.941a7d5c.chunk.css"
+  },
+  {
+    "revision": "381877d9f450cf397fd6",
+    "url": "/static/js/2.bf058b54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bf058b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8999acc56ade91e08cd",
+    "url": "/static/js/main.a42519a2.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  }
+]);
